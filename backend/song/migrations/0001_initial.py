5216819a4243ddd191b8422c851635ca120fa7ae
Pavel Polyarus <keytohockey@gmail.com>
2025-02-15 18:52:51 +0300

    minor changes

--- backend/song/migrations/0001_initial.py
+++ backend/song/migrations/0001_initial.py
@@ -16,6 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=255)),
+                ('cover', models.URLField()),
                 ('year', models.IntegerField()),
                 ('yandex_music_link', models.URLField()),
             ],
