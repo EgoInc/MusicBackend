5765646570ed970bb801a1c9a44ef01f74236991
Marina Mashkovtseva <102848919+Marinchanka@users.noreply.github.com>
2025-02-20 19:42:06 +0300

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('clips.urls')),
     path('api/', include('merch.urls')),
+    path("api/", include("concerts.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
