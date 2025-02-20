cf72d2b09e61c3245786da2ba1d49b87d13f8120
EgoInc <56895607+EgoInc@users.noreply.github.com>
2025-02-20 18:14:41 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "drf_spectacular",
+    'clips',  # <-- Добавь сюда свои приложения
+    'concerts',
     'merch',
+    'song',
 ]
 
 MIDDLEWARE = [
