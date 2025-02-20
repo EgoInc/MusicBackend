d17fab5c556c830a17218b41cf50cab57f8b18e6
Marina Mashkovtseva <102848919+Marinchanka@users.noreply.github.com>
2025-02-20 19:38:00 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
